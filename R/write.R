0c90ee82c64fcd11231c7df37623b99395bde67d
Joe Thorley <joe@poissonconsulting.ca>
2018-11-20 13:56:41 -0800

    document write.csv

--- R/write.R
+++ R/write.R
@@ -1,6 +1,45 @@
 #' Write csv
 #'
-#' @param data The data frame with minimum of columnms Station and DateTime
+#' The initial columns in the csv file are as follows
+#' \describe{
+#'   \item{Year}{The year of the value.}
+#'   \item{Month}{The month of the value.}
+#'   \item{Day}{The day of the value.}
+#'   \item{Hour}{The hour of the value.}
+#'   \item{Station}{The station}
+#' }
+#' The csv file should also include the following columns
+#' \describe{
+#'   \item{Recorded}{The value as provided by the station maintainer.}
+#'   \item{Corrected}{The corrected value (see below).}
+#'   \item{Status}{The reliability of the corrected value (see below).}
+#'   \item{Parameter}{The parameter type, ie, Discharge, Elevation, Water Temperature etc.}
+#'   \item{Units}{The units of the value.}
+#'   \item{StationName}{The name of the station.}
+#'   \item{Comments}{Comments on the value.}
+#' }
+#' 
+#' There are three possible status values
+#' \describe{
+#'   \item{Reasonable}{There is nothing obviously wrong with the value.}
+#'   \item{Questionable}{The value may be wrong.}
+#'   \item{Erroneous}{The value is definitely wrong.}
+#' }
+#' In general questionable values are those which are in the range of possible 
+#' values but are inconsistent with spatial or temporal neighbours 
+#' while erroneous value are those which are outside the range of possible values, ie negative discharge.
+#' 
+#' If a value is erroneous, questionable or missing (indicated by NA) 
+#' then we will (time permitting) provide a corrected value based on neighbouring values.
+#' Otherwise the corrected value is simply the recorded value.
+#' The status column refers to the corrected value and 
+#' corrected values which differ from the recorded value are coded as Questionable.
+#' 
+#' If data gaps are not important it is recommended that you only include Reasonable values.
+#' Otherwise it is recommended that you only include Questionable values 
+#' that you consider to be reliable based on plotting and analysis.
+#' 
+#' @param data The data frame with a minimum of columns Station and DateTime
 #' @inheritParams utils::write.csv
 #' @return An invisible copy of saved data.
 #' @export
